dd3ddbb6af582c8031c7be245de0e1229bde8e8f
Alex O <saninstein@gmail.com>
2020-02-20 19:00:56 +0300

    Update simplesum.py

--- simplesum/simplesum.py
+++ simplesum/simplesum.py
@@ -1,23 +1,24 @@
 from typing import List, Union
 
+
 def sum(x: List) -> Union[int, float]:
     """
     Sum of array items
-    
+
     Parameters
     ----------
     x : list
         Array
-    
+
     Returns
     -------
     Union[int, float]
         Result of sum
-    
+
     Examples
     -------
     >>> sum([1, 2])
     3
     """
-    
+
     return sum(x)
