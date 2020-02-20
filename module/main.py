5745157cf484f84c786c8f3b55d524022a75b8f2
Alex O <saninstein@gmail.com>
2020-02-20 18:38:24 +0300

    Create main.py

---
+++ module/main.py
@@ -0,0 +1,23 @@
+from typing import List, Union
+
+def sum(x: List) -> Union[int, float]:
+    """
+    Sum of array items
+    
+    Parameters
+    ----------
+    x : list
+        Array
+    
+    Returns
+    -------
+    Union[int, float]
+        Result of sum
+    
+    Examples
+    -------
+    >>> sum([1, 2])
+    3
+    """
+    
+    return sum(x)
