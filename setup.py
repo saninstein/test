dcba530a72ad8f20cd8c6b149b3e29bbc1fc88e9
Alex O <saninstein@gmail.com>
2020-02-20 18:44:48 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
+
+pip_session = PipSession()
+
+
+def parse_reqs(path):
+    return [str(r.req) for r in parse_requirements(path, session=pip_session)]
+
+
+setup(
+    name=simplesum,
+    packages=find_packages(include=["simplesum*"]),
+    extras_require={
+        "dev": parse_reqs("requirements.txt"),
+    },
+)
